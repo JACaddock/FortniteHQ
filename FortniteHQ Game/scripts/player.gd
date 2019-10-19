01603ce31ed827913323114e2ad82c1ed272061e
DoctorCurse <jcaddock@virginmedia.com>
2019-10-19 22:54:03 +0100

    player script

--- FortniteHQ Game/scripts/player.gd
+++ FortniteHQ Game/scripts/player.gd
@@ -1,30 +1,52 @@
 extends KinematicBody2D
 
-const SPEED = 200
+var speed = 200
+#const scene_up = Vector2(0, -1)
+const JUMP_POWER = -250
+const GRAVITY = 10
+const FLOOR = Vector2(0, -1)
+var onGround = false
+var velocity = Vector2()
+#var direction = 1
+var hasHealth = true
+var health = 2
 
-var movedir = Vector2(0,0)
+
+func get_input():                
+    if Input.is_action_pressed('ui_right'):
+        velocity.x = speed
+        $Sprite.flip_h = false
+        
+    elif Input.is_action_pressed("ui_left"):
+        velocity.x = -speed
+        $Sprite.flip_h = true
+        
+    else:
+        velocity.x = 0
+        
+    if Input.is_action_just_pressed("jump"):
+        if onGround:
+            velocity.y = JUMP_POWER
+            onGround = false
+
 
 func _physics_process(delta):
-    controls_loop()
-    movement_loop()
+    get_input()
     
-    if movedir != Vector2(0,0):
-        anim_switch("walk")
+    if velocity.x != 0:
+        anim_switch("Walk")
     else:
-        anim_switch("idle")
-
-
-func controls_loop():
-    var LEFT = Input.is_action_pressed("ui_left")
-    var RIGHT = Input.is_action_pressed("ui_right")
+        anim_switch("Still")
+        
+    velocity.y += GRAVITY + (delta * 10)
     
-    movedir.x = -int(LEFT) + int(RIGHT)
-    
-    
-func movement_loop():
-    var motion = movedir.normalized() * SPEED
-    move_and_slide(motion, Vector2(0,-1))
-
+    if is_on_floor():
+        onGround = true
+    else:
+        onGround = false
+        
+    velocity = move_and_slide( velocity, FLOOR )
+            
 
 func anim_switch(animation):
     var newanim = str(animation)
